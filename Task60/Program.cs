4f57cf68f0692eca7d33dc29697400a09fcba026
Anastasia <anastbulokhova@gmal.com>
2023-09-29 17:10:03 +0300

    Task60

---
+++ Task60/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 60. Сформируйте трёхмерный массив . Напишите программу, которая будет построчно
+// выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int[,,] Get3Dmatrix(int rows, int columns, int hight, int minValue = 0, int maxValue = 10)
+{
+    int[,,] matrix = new int[rows, columns, hight];
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = rand.Next(minValue, maxValue + 1);;
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintIndex(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine();
+
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+int[,,] matrix = Get3Dmatrix(2,2,2);
+PrintIndex(matrix);
